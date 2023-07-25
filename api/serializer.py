abd11fb4876ec5115eee45cfb971b462df8eeed0
dnogues89 <damiannogues@gmail.com>
2023-07-25 17:51:04 -0300

    listado tareas en api

--- api/serializer.py
+++ api/serializer.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from tareas.models import  Preventa
+from tareas.models import  Preventa, Tareas
+
+class TareasSerializer(serializers.Serializer):
+    class Meta:
+        model = Tareas
+        fields = '__all__'
+        depth = 1
 
 class PreventaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Preventa
         fields = '__all__'
-        
-    depth = 1
+        depth = 1
     
