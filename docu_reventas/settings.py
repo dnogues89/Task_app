382c6ba48a025b92b3aae5f2a303b8d5982d911d
dnogues89 <damiannogues@gmail.com>
2023-08-16 09:56:27 -0300

    static files

--- docu_reventas/settings.py
+++ docu_reventas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ixo)^-nos9n*zyuo3v&fv_^pldh%munfx=kz^4s74sio(pe4-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tareas.apps.TareasConfig',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
