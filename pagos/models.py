cf0598cbbe0ca33f17e475c98f1e525ca0ceb428
dnogues89 <damiannogues@gmail.com>
2023-08-23 22:29:12 -0300

    corrijo bug a la hora de actualizar un comprobante

--- pagos/models.py
+++ pagos/models.py
@@ -7,6 +7,8 @@ from django.utils.html import format_html
 import os
 
 def save_path(instance,filename):
+    print('estoy aca')
+    print(filename)
     if instance.preventa is not None or instance.preventa != '':
         return f'pagos/{instance.preventa_id}/{filename}'
     return f'pagos/{filename}'
@@ -34,7 +36,7 @@ class Pago(models.Model):
     
     def save(self, *args, **kwargs):
             try:
-                self.comprobante.name = f'{self.numero_comprobante}.{self.comprobante.name.split(".")[-1]}'
+                
                 if self.pk is not None:   
                     # Obtener el objeto existente de la base de datos.
                     old_obj = Pago.objects.get(pk=self.pk)
@@ -42,12 +44,8 @@ class Pago(models.Model):
                     if old_obj.comprobante != self.comprobante:
                         # Eliminar el archivo adjunto anterior si ha sido modificado.
                         old_obj.comprobante.delete(save=False)
-                        _,ext = os.path.splitext(os.path.basename(self.comprobante.name))
-                        self.comprobante.name = f'{self.numero_comprobante}{ext}'
-                        print(self.comprobante)
-                        print(self.comprobante.name)
-                    
-                    
+                        self.comprobante.name = f'{self.numero_comprobante}.{self.comprobante.name.split(".")[-1]}' 
+
                     if self.estado == '2Aprobado':
                         if old_obj != None and old_obj.estado != '2Aprobado':
                             self.fecha_aprobado = timezone.now()
@@ -56,7 +54,8 @@ class Pago(models.Model):
                         self.estado == '1Pendiente'
                     
             except:
-                pass    
+                pass   
+            
             # Guardar la instancia del modelo con el nuevo archivo adjunto.
             super().save(*args, **kwargs)
     
